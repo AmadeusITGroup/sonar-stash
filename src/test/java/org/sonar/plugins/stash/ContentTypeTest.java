f8c28dbbebd8ecb57b215d27eaaae886ec2ffb80
Thomas Weißschuh <thomas.weissschuh@de.amadeus.com>
2016-09-26 14:30:39 +0000

    more testing

--- src/test/java/org/sonar/plugins/stash/ContentTypeTest.java
+++ src/test/java/org/sonar/plugins/stash/ContentTypeTest.java
@@ -12,6 +12,10 @@ public class ContentTypeTest {
         Assert.assertTrue(json.match("APPLICATION/JSON"));
         Assert.assertTrue(json.match("ApPlIcAtIoN/jSoN"));
 
+        Assert.assertFalse(json.match(""));
+        Assert.assertFalse(json.match("/"));
+        Assert.assertFalse(json.match(";"));
+        Assert.assertFalse(json.match("/;"));
         Assert.assertFalse(json.match("foo"));
         Assert.assertFalse(json.match("12!4"));
         Assert.assertFalse(json.match("foo/json"));
