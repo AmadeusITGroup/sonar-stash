2bd6e8486dbbb2148b7731e8aa4d7b8d6f4aaf05
Thomas Weißschuh <thomas.weissschuh@de.amadeus.com>
2016-09-26 11:48:30 +0000

    accept json responses with parameters
    Some services use application/json with the charset= parameter.
    This violates the spec but we handle it gracefully.
    
    closes #71 and #72

---
+++ src/test/java/org/sonar/plugins/stash/ContentTypeTest.java
@@ -0,0 +1,21 @@
+package org.sonar.plugins.stash;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ContentTypeTest {
+    @Test
+    public void testContentType() {
+        ContentType json = new ContentType("application", "json", null);
+        Assert.assertTrue(json.match("application/json"));
+        Assert.assertTrue(json.match("application/json;charset=utf-8"));
+        Assert.assertTrue(json.match("APPLICATION/JSON"));
+        Assert.assertTrue(json.match("ApPlIcAtIoN/jSoN"));
+
+        Assert.assertFalse(json.match("foo"));
+        Assert.assertFalse(json.match("12!4"));
+        Assert.assertFalse(json.match("foo/json"));
+        Assert.assertFalse(json.match("application/foo"));
+        Assert.assertFalse(json.match("application/foo;charset=utf-8"));
+    }
+}
