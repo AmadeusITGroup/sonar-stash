8596c66f8126e4e9968dcb812e864e80ecd38eb2
Oleg Marchuk <oleg_m@xwiserd.com>
2017-03-27 15:55:42 +0300

    Add logging

--- src/main/java/org/sonar/plugins/stash/StashProjectBuilder.java
+++ src/main/java/org/sonar/plugins/stash/StashProjectBuilder.java
@@ -1,15 +1,21 @@
 package org.sonar.plugins.stash;
 
-import org.sonar.api.batch.bootstrap.ProjectBuilder;
-
 import java.io.File;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.sonar.api.batch.bootstrap.ProjectBuilder;
+
 public class StashProjectBuilder extends ProjectBuilder {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(StashProjectBuilder.class);
+
   private File workingDir;
 
   @Override
   public void build(Context context) {
     workingDir = new File(System.getProperty("user.dir"));
+    LOGGER.debug("Current working directory {}", workingDir);
   }
 
   public File getWorkingDir() {
