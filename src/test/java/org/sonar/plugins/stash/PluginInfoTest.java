26b5aea55da945f4d76df73ee43c0ec51b5d61a3
remi.schneider <remi.schneider@amadeus.com>
2017-06-08 15:20:34 +0200

    COVERAGE: adding unit-tests for PluginInfo

---
+++ src/test/java/org/sonar/plugins/stash/PluginInfoTest.java
@@ -0,0 +1,24 @@
+package org.sonar.plugins.stash;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PluginInfoTest {
+
+    private static final String plugin_name = "sonar-stash";
+    private static final String plugin_vers = "1337.0";
+
+
+    @Test
+    public void testPluginInfoRef_ConstructorAndAccessors() {
+
+        PluginInfo PI = new PluginInfo(plugin_name, plugin_vers);
+
+        assertEquals(plugin_name, PI.getName());
+        assertEquals(plugin_vers, PI.getVersion());
+    }
+
+}
