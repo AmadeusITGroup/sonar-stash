52693b96dd71083f2f60035092f4747c33b4f938
remi.schneider <remi.schneider@amadeus.com>
2017-06-14 10:56:20 +0200

    StashPluginUtils: making sure the formatPercentage() works consistently in a non-US locale env

--- src/main/java/org/sonar/plugins/stash/StashPluginUtils.java
+++ src/main/java/org/sonar/plugins/stash/StashPluginUtils.java
@@ -2,6 +2,7 @@ package org.sonar.plugins.stash;
 
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,7 +15,16 @@ public final class StashPluginUtils {
   private StashPluginUtils() {}
 
   public static String formatPercentage(double d) {
+
+    // Defining that our percentage is precise down to 0.1%
     DecimalFormat df = new DecimalFormat("0.0");
+    
+    // Protecting this method against non-US locales that would not use '.' as decimal separation
+    DecimalFormatSymbols decimalFormatSymbols = new DecimalFormatSymbols();
+    decimalFormatSymbols.setDecimalSeparator('.');
+    df.setDecimalFormatSymbols(decimalFormatSymbols);
+
+    // Making sure that we round the 0.1% properly out of the double value
     df.setRoundingMode(RoundingMode.HALF_UP);
     return df.format(d);
   }
